c71c7c44e7834859bf8bacb32ad3f877dee68273
Renan <renanssantini@hotmail.com>
2021-06-28 20:45:12 -0300

    Implementação do Factory Method

---
+++ 01 - Creational/1.2 - Factory Method/ConcreteCreator1.cs
@@ -0,0 +1,10 @@
+namespace DesingPartterns.FactoryMethod
+{
+    public class ConcreteCreator1 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct1();
+        }
+    }
+}
---
+++ 01 - Creational/1.2 - Factory Method/Creator.cs
@@ -0,0 +1,17 @@
+namespace DesingPartterns.FactoryMethod
+{
+    public abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+
+        public string SomeOperation()
+        {
+            var product = FactoryMethod();
+
+            var result = "Creator: The same creator's code has just worked with"
+            + product.Operation();
+
+            return result;
+        }
+    }
+}
